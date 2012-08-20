541384ef69efdece9dbc53209d0536f67b1c15ac
AstinCHOI <asciineo@gmail.com>
2012-08-20 11:32:26 +0900

    [34608049] add model with group and chat

--- hive/chats/models.py
+++ hive/chats/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Messages(models.Model):
+    sender = models.ForeignKey(User)
+    recevier = models.ForeignKey(User)
+    message = models.CharField()
+    write_time = models.DateTimeField(auto_now_add=True)
--- hive/hgroups/models.py
+++ hive/hgroups/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GroupProfile(models.Model):
+    GROUP_KINDS = (
+        ('MU', 'Music'),
+        ('IT', 'Information Technology'),
+    )
+
+    name = models.CharField()
+    owner = models.CharField()
+    gimage = models.ImageField(null=True)
+    kinds = models.CharField(choices=GROUP_KINDS)
+    create_time = models.DataTimeField(auto_now_add=True)
+
+class UserList(models.Model):
+    hgroup = models.ForeignKey(GroupProfile)
+    user = models.ForeignKey(User)
+
