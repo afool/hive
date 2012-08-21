097d353c95918aa2d91c6ed53b22a465236fae00
Sinhyub <sinhyub@gmail.com>
2012-08-21 18:13:14 +0900

    try build timeline

--- hive/timelines/views.py
+++ hive/timelines/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from posts.models import Post
+from django.core.cache import cache
+
+def timeline_index(request):
+#    request.session.
+#    cache.get('')
+    post_list = Post.objects.order_by('create_time')[:5]
+    return render_to_response('timeline/index.html', 
+                              {'post_list': post_list})
+
