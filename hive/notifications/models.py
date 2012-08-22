90f8f6c93b1fc3d97319bd6e526692d746b64efd
AstinCHOI <asciineo@gmail.com>
2012-08-22 17:25:49 +0900

    [#34608055] model notification added

--- hive/notifications/models.py
+++ hive/notifications/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hannouncment(models.Model):
+    ANNOUNCEMENTS_KINDS = (
+
+    )
+    notifier = models.ForeignKey(User)
+    notifiee = models.ForeignKey(User)
+    notificated_time = models.DateTimeField(auto_now_add=True)
+    new_announcements = models.IntegerField()
+    announcement = models.CharField()
+    kinds = models.CharField(choices=ANNOUNCEMENTS_KINDS)
+
+    class __unicode__(self):
+        pass
+
+    class Meta:
+        ordering = ['notificated_time']
+
+
+class Hmessage(models.Model):
+    MESSAGES_KINDS = (
+
+    )
+    notifier = models.ForeignKey(User)
+    notifiee = models.ForeignKey(User)
+    notificated_time = models.DateTimeField(auto_now_add=True)
+    new_announcements = models.IntegerField()
+    message = models.CharField()
+    kinds = models.CharField(choices=MESSAGES_KINDS)
+
+    class __unicode__(self):
+        pass
+
+    class Meta:
+        ordering = ['notificated_time']
+
+class HRequest(models.Model):
+    REQUESTS_KINDS = (
+
+    )
+    requester = models.ForeignKey(User)
+    requestee = models.ForeignKey(User)
+    notificated_time = models.DateTimeField(auto_now_add=True)
+    new_requestments = models.IntegerField()
+    kinds = models.CharField(choices=REQUEST_KINDS)
+
+    class __unicode__(self):
+        pass
+
+    class Meta:
+        ordering = ['notificated_time']
+
+
