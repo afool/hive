6cfaa958e08debf8d525bbedff976ee2cdb27d49
KwonHan Bae <darjeeling@gmail.com>
2012-08-22 12:57:28 +0900

    add applications to settings.py

--- hive/hive/settings.py
+++ hive/hive/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hive.sq3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -149,6 +149,13 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'admins',
+    'accounts',
+    'chats',
+    'hgroups',
+    'notifications',
+    'timelines',
+    'posts',
 )
 
 # A sample logging configuration. The only tangible logging
