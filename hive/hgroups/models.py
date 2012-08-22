d6ddabd464c288795bd31e9b08ab3357a5394f58
AstinCHOI <asciineo@gmail.com>
2012-08-22 17:33:46 +0900

    [#34608049] hgroups model add: Meta, __unicode__ put pass

--- hive/hgroups/models.py
+++ hive/hgroups/models.py
@@ -14,10 +14,22 @@ class GroupProfile(models.Model):
     kinds = models.CharField(choices=GROUP_KINDS, max_length=50)
     create_time = models.DateTimeField(auto_now_add=True)
     is_public = models.BooleanField()
-    
+
     members = models.ManyToManyField(User, through='Membership', related_name="group_members")
+
+    class __unicode__(self):
+        pass
+
+    class Meta:
+        pass
 
 class Membership(models.Model):
     person = models.ForeignKey(User)
     group = models.ForeignKey(GroupProfile)
     date_joined = models.DateField()
+
+    class __unicode__(self):
+        pass
+
+    class Meta:
+        pass
