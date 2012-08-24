c2cd160186724adff9a6f75ce9a640d995cd0297
AstinCHOI <asciineo@gmail.com>
2012-08-25 02:21:07 +0900

    [#34608049] update hgroup and notifications

--- hive/notifications/models.py
+++ hive/notifications/models.py
@@ -8,12 +8,12 @@ class Hannouncment(models.Model):
     notifier = models.ForeignKey(User)
     notifiee = models.ForeignKey(User, related_name='notification_announcement')
     notificated_time = models.DateTimeField(auto_now_add=True)
-    new_announcements = models.IntegerField()
+    confirmed = models.BooleanField()
     announcement = models.CharField(max_length=120)
     kinds = models.CharField(choices=ANNOUNCEMENTS_KINDS, max_length=30)
 
     class Meta:
-        ordering = ['notificated_time']
+        pass
 
     def __unicode__(self):
         pass
@@ -23,15 +23,16 @@ class Hmessage(models.Model):
     MESSAGES_KINDS = (
 
     )
+
     chater = models.ForeignKey(User)
     chatee = models.ForeignKey(User, related_name='notification_message')
     notificated_time = models.DateTimeField(auto_now_add=True)
-    new_announcements = models.IntegerField()
+    confirmed = models.BooleanField()
     message = models.CharField(max_length=120)
     kinds = models.CharField(choices=MESSAGES_KINDS, max_length=30)
 
     class Meta:
-        ordering = ['notificated_time']
+        pass
 
     def __unicode__(self):
         pass
@@ -40,14 +41,15 @@ class HRequest(models.Model):
     REQUESTS_KINDS = (
 
     )
+
     requester = models.ForeignKey(User)
     requestee = models.ForeignKey(User, related_name='notification_request')
     notificated_time = models.DateTimeField(auto_now_add=True)
-    new_requestments = models.IntegerField()
+    confirmed = models.IntegerField()
     kinds = models.CharField(choices=REQUESTS_KINDS, max_length=30)
 
     class Meta:
-        ordering = ['notificated_time']
+        pass
 
     def __unicode__(self):
         pass
