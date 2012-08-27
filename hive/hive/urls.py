7792fadd122843cd7d33600d3b25da93d16632d8
AstinCHOI <asciineo@gmail.com>
2012-08-27 23:58:48 +0900

    [#35002457] E-mail activation with park

--- hive/hive/urls.py
+++ hive/hive/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', direct_to_template, { 'template': 'main/index.html' }),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^admins/', include('admins.urls')),
