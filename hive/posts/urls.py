b582bb22c27959ebf05665494aa2f497a3719b03
Acuros Jr <acuros.jr@sun-riser.com>
2012-08-30 23:29:29 +0900

    add test attachment view

--- hive/posts/urls.py
+++ hive/posts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('posts.views',
     url(r'(?P<posts_id>\d+)/$', 'one_of_posts_detail'),
     url(r'create/$', 'create_post'),
-    url(r'create_from_timeline/$', 'create_post_timeline')
+    url(r'create_from_timeline/$', 'create_post_timeline'),
+    url(r'test/','test_page')
 )
