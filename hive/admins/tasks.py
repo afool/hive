c31d80a8929cba058abcbf408f55ea24343c4d85
Sinhyub <sinhyub@gmail.com>
2012-08-31 01:41:13 +0900

    [#35235811][#35230009] add Celery to project, implement activities analyze task, implement admin command

---
+++ hive/admins/tasks.py
@@ -0,0 +1,25 @@
+from celery import task
+from posts.models import Post, Like, Comment
+from admins.models import ActivitiesInformation
+import datetime
+
+@task(name='execute_analyze_activities')
+def execute_analyze_activities():
+    num_of_posts = Post.objects.all().count()
+    num_of_likes = Like.objects.all().count()
+    num_of_comments = Comment.objects.all().count()
+    
+    ActivitiesInformation.objects.create(date = datetime.datetime.today(),
+                                         num_posts = num_of_posts,
+                                         num_likes = num_of_likes,
+                                         num_comments = num_of_comments,
+                                         num_web_feeds = 0,
+                                         num_messages = 0,
+                                          )
+    excute_again()
+    execute_analyze_activities.name
+    
+def excute_again():
+    execute_analyze_activities.apply_async(countdown=3)
+
+
