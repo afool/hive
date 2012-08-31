691d9f2184be5279ce849a425af3532c067927fb
KwonHan Bae <darjeeling@gmail.com>
2012-08-31 16:11:41 +0900

    modify settings.MEDIA_ROOT

--- hive/hive/settings.py
+++ hive/hive/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "media/")
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
