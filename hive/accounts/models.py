5327475df0dc34fd10516e3cf542ff63fe458ad5
AstinCHOI <asciineo@gmail.com>
2012-09-06 02:01:32 +0900

    [#35535059] Sort Alphabetically accounts

--- hive/accounts/models.py
+++ hive/accounts/models.py
@@ -1,40 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.template.loader import render_to_string
 
-class UserProfile(models.Model):
-    EMOTION_CHOICES = (
-        ('H', 'Happy'),
-        ('S', 'Sad')
-    )
-
-    user = models.OneToOneField(User) # User Profile have to contain this field.
-    department = models.CharField(null=True, max_length=50)
-    position = models.CharField(null=True, max_length=50)
-    emotion = models.CharField(null=True, choices=EMOTION_CHOICES, max_length=20)
-    portrait = models.FileField(null=True, upload_to="user_profile/")
-    phone = models.CharField(null=True, max_length=50)
-    
-    class Meta:
-        ordering = ['user__username']
-        
-    def __unicode__(self):
-        return "%s's profile" %(self.user.username)
-    
-    def get_absolute_url(self):
-        return '/accounts/profile/%s' %(self.user.username)
-    
-    def get_rendered(self):
-        return render_to_string('accounts/people_profile_render.html',{
-                                                                       'people_profile':self
-                                                                       })
-
 class EmailActivation(models.Model):
+    activation_key = models.CharField(max_length=40)
     email = models.EmailField(unique=True)
     expire_date = models.DateTimeField()
-    activation_key = models.CharField(max_length=40)
     #activation_user = models.ForeignKey(User)
-
 
 class Following(models.Model):
     followee = models.ForeignKey(User, related_name = 'following_followee')
@@ -51,3 +23,31 @@ class Following(models.Model):
         
     def get_absolute_url(self):
         return "/accounts/%s/followings/%d" %(self.followee, self.id)
+
+class UserProfile(models.Model):
+    EMOTION_CHOICES = (
+        ('H', 'Happy'),
+        ('S', 'Sad')
+    )
+
+    department = models.CharField(null=True, max_length=50)
+    emotion = models.CharField(null=True, choices=EMOTION_CHOICES, max_length=20)
+    phone = models.CharField(null=True, max_length=50)
+    portrait = models.FileField(null=True, upload_to="user_profile/")
+    position = models.CharField(null=True, max_length=50)
+    user = models.OneToOneField(User) # User Profile have to contain this field.
+    
+    
+    class Meta:
+        ordering = ['user__username']
+        
+    def __unicode__(self):
+        return "%s's profile" %(self.user.username)
+    
+    def get_absolute_url(self):
+        return '/accounts/profile/%s' %(self.user.username)
+    
+    def get_rendered(self):
+        return render_to_string('accounts/people_profile_render.html',{
+                                                                       'people_profile':self
+                                                                       })
--- hive/accounts/views.py
+++ hive/accounts/views.py
@@ -1,31 +1,59 @@
+from accounts.models import UserProfile, Following
+from forms import UserRegistrationForm
+from hive import settings 
+from models import EmailActivation
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm
 from django.contrib.auth.models import User
-from accounts.models import UserProfile, Following
+#from django.core.cache import cache
+from django.core.mail import BadHeaderError, send_mail
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponseRedirect, HttpResponse
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.core.validators import validate_email
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.core.validators import validate_email
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from forms import UserRegistrationForm
 
-from models import EmailActivation
 import md5, time, datetime
-from hive import settings 
-# from django.core.cache import cache
-# from accounts.models import *
+
+
+def add_follow_page(request, followee_id):
+    followee = User.objects.get(id=followee_id )
+    if not Following.objects.filter(followee=followee, follower =request.user ).count():
+        Following.objects.create(followee=followee, follower =request.user )
+    return HttpResponse("OK")
+
+
+def activation_page(request, key):
+    try:
+        user = EmailActivation.objects.get(activation_key=key)
+    except ObjectDoesNotExist:
+        status = 'Invalid access'
+        return render_to_response('accounts/login.html',
+                                           RequestContext(request,
+                                                          {'form': AuthenticationForm(),
+                                                           'status': status}))
+    if user and user.expire_date.date() >= datetime.datetime.today().date():
+        form = UserRegistrationForm()
+        return render_to_response('accounts/user_registration.html',
+                                   RequestContext(request, {
+                                    'form': form,
+                                    'key': key,
+                                    },))
+    else:
+        status = 'Invalid access'
+        return render_to_response('accounts/login.html',
+                                           RequestContext(request,
+                                                          {'form': AuthenticationForm(),
+                                                           'status': status}))
+
 
 def email_register_page(request):
     LETTER = '''Hello,\
         \nWelcome to HIVE!!\
         \nYou have to go %s%s for the activation.\
         \nGood Luck!\n\n- Hive, A fool team-'''
-    def _keygen(email):
-        email_key = md5.md5()
-        email_key.update(email + str(time.time()))
-        return email_key.hexdigest()
 
     def _is_registered(email):
         # Before registering user, we have to check both -
@@ -44,13 +72,18 @@ def email_register_page(request):
         except ObjectDoesNotExist:
             return True
         
-
     def _is_validated(email):
         try:
             validate_email(email)
             return True
         except ValidationError:
             return False
+        
+    def _keygen(email):
+        email_key = md5.md5()
+        email_key.update(email + str(time.time()))
+        return email_key.hexdigest()
+
     
     if request.method == 'POST':
         try:
@@ -87,68 +120,13 @@ def email_register_page(request):
                                                            'status': status}))
 
     return HttpResponseRedirect('/')
-    
 
-def activation_page(request, key):
-    try:
-        user = EmailActivation.objects.get(activation_key=key)
-    except ObjectDoesNotExist:
-        status = 'Invalid access'
-        return render_to_response('accounts/login.html',
-                                           RequestContext(request,
-                                                          {'form': AuthenticationForm(),
-                                                           'status': status}))
-    if user and user.expire_date.date() >= datetime.datetime.today().date():
-        form = UserRegistrationForm()
-        return render_to_response('accounts/user_registration.html',
-                                   RequestContext(request, {
-                                    'form': form,
-                                    'key': key,
-                                    },))
-    else:
-        status = 'Invalid access'
-        return render_to_response('accounts/login.html',
-                                           RequestContext(request,
-                                                          {'form': AuthenticationForm(),
-                                                           'status': status}))
-# 340a1de9a00b30b5003f7057f167bf61
-def register_userinfo_page(request, key):
-    if request.method != "POST" :
-        return HttpResponseRedirect('/')
-    else:
-        userinfo_form = UserRegistrationForm(request.POST)
-        
-        if userinfo_form.is_valid():
-            try:
-                user = EmailActivation.objects.get(activation_key=key)
-                
-                userinfo_form.clean_username()
-                userinfo_form.clean_password2()
-                new_user = userinfo_form.save()
-                
-                new_user.email = user.email
-                new_user.save() # Enroll Email
-                user.delete() # Delete Activation Key
-                
-                UserProfile.objects.create(user=new_user)
-                Following.objects.create(followee=new_user,
-                                         followee_str = new_user.username,
-                                         follower=new_user,
-                                         follower_str = new_user.username)
-            except ObjectDoesNotExist:
-                return HttpResponseRedirect('/')    
-                        
-        return HttpResponseRedirect('/')            
 
 def forgot_password_page(request):
     LETTER = '''Hello,\
         \nYou've forgot the password!!\
         \nYou have to go %s%s for the renew.\
         \nGood Luck!\n\n- Hive, A fool team-'''
-    def _keygen(email):
-        email_key = md5.md5()
-        email_key.update(email + str(time.time()))
-        return email_key.hexdigest()
 
     def _is_registered(email):
         try:
@@ -163,6 +141,12 @@ def forgot_password_page(request):
             return True
         except ValidationError:
             return False
+        
+    def _keygen(email):
+        email_key = md5.md5()
+        email_key.update(email + str(time.time()))
+        return email_key.hexdigest()
+
         
     if request.method == 'POST':
         try:
@@ -200,48 +184,8 @@ def forgot_password_page(request):
 
     return HttpResponseRedirect('/')
 
-def renew_password_email_page(request, key):
-    try:
-        user_act = EmailActivation.objects.get(activation_key=key)
-        user = User.objects.get(email=user_act.email)
-        form = SetPasswordForm(forms.Form)
-    except ObjectDoesNotExist:
-        return HttpResponseRedirect('/')
-    
-    return render_to_response('accounts/renew_password.html',
-                                       RequestContext(request,
-                                                      {'form': form,
-                                                       'key': key}))
 
-def renew_password_page(request, key):
-    if request.method != "POST" :
-        return HttpResponseRedirect('/')
-    else:
-        user_act = EmailActivation.objects.get(activation_key=key)
-        user = User.objects.get(email=user_act.email) 
-        user_form = SetPasswordForm(request.POST)
-        if user_form.is_valid():
-            user_form.clean_new_password2()
-            user_form.save()
-            try:
-                user_act = EmailActivation.objects.get(activation_key=key)
-                user_act.delete() # Delete Activation Key
-            except ObjectDoesNotExist:
-                return HttpResponseRedirect('/')    
-                        
-        return HttpResponseRedirect('/')
     
-def profile_page(request, username):
-    user = None
-    if username is None:
-        user = request.user
-    else :
-        user = User.objects.get(username=username)
-    user_profile = UserProfile.objects.get(user = user)
-    return render_to_response('accounts/detail_profile.html', RequestContext(request,{
-                                                               'user' : user,
-                                                               'user_profile':user_profile,
-                                                               }))
 
 def people_list_page(request):
     PAGE_SIZE = 20
@@ -272,14 +216,86 @@ def people_list_page(request):
                                            'peoples':peoples,
                                            'observer':observer,
                                            'url_search_param':url_search_param, }))
+    
+    
+    
+def profile_page(request, username):
+    user = None
+    if username is None:
+        user = request.user
+    else :
+        user = User.objects.get(username=username)
+    user_profile = UserProfile.objects.get(user = user)
+    return render_to_response('accounts/detail_profile.html', RequestContext(request,{
+                                                               'user' : user,
+                                                               'user_profile':user_profile,
+                                                               }))
 
-def add_follow_page(request, followee_id):
-    followee = User.objects.get(id=followee_id )
-    if not Following.objects.filter(followee=followee, follower =request.user ).count():
-        Following.objects.create(followee=followee, follower =request.user )
-    return HttpResponse("OK")
+
+def register_userinfo_page(request, key):
+    if request.method != "POST" :
+        return HttpResponseRedirect('/')
+    else:
+        userinfo_form = UserRegistrationForm(request.POST)
+        
+        if userinfo_form.is_valid():
+            try:
+                user = EmailActivation.objects.get(activation_key=key)
+                
+                userinfo_form.clean_username()
+                userinfo_form.clean_password2()
+                new_user = userinfo_form.save()
+                
+                new_user.email = user.email
+                new_user.save() # Enroll Email
+                user.delete() # Delete Activation Key
+                
+                UserProfile.objects.create(user=new_user)
+                Following.objects.create(followee=new_user,
+                                         followee_str = new_user.username,
+                                         follower=new_user,
+                                         follower_str = new_user.username)
+            except ObjectDoesNotExist:
+                return HttpResponseRedirect('/')    
+                        
+        return HttpResponseRedirect('/')            
+
 
 def remove_follow_page(request, followee_id):
     followee = User.objects.get(id=followee_id )
     Following.objects.filter(followee=followee, follower =request.user ).all().delete()
     return HttpResponse("OK")
+
+
+def renew_password_email_page(request, key):
+    try:
+        user_act = EmailActivation.objects.get(activation_key=key)
+        user = User.objects.get(email=user_act.email)
+        form = SetPasswordForm(forms.Form)
+        form.__init__(user=user)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect('/')
+    
+    return render_to_response('accounts/renew_password.html',
+                                       RequestContext(request,
+                                                      {'form': form,
+                                                       'key': key}))
+
+
+def renew_password_page(request, key):
+    if request.method != "POST" :
+        return HttpResponseRedirect('/')
+    else:
+        user_act = EmailActivation.objects.get(activation_key=key)
+        user = User.objects.get(email=user_act.email) 
+        user_form = SetPasswordForm(request.POST)
+        if user_form.is_valid():
+            user_form.clean_new_password2()
+            user_form.save()
+            try:
+                user_act = EmailActivation.objects.get(activation_key=key)
+                user_act.delete() # Delete Activation Key
+            except ObjectDoesNotExist:
+                return HttpResponseRedirect('/')    
+                        
+        return HttpResponseRedirect('/')
