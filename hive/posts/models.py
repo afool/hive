6815d6dc691a55b9cad3a72b7f1d55ea4e2672b2
AstinCHOI <asciineo@gmail.com>
2012-09-10 17:49:25 +0900

    change datetime to auto_now_add

--- hive/posts/models.py
+++ hive/posts/models.py
@@ -1,12 +1,7 @@
-#from timelines.models import FollowerList, Timeline
-
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import F
-from django.http import Http404
 from django.template.loader import render_to_string
-
-import datetime
 
 
 class Post(models.Model):    
@@ -84,7 +79,7 @@ class Post(models.Model):
 
 class Attachment(models.Model):
     file_name = models.CharField(max_length=100)
-    create_time = models.DateTimeField(default=datetime.datetime.now)
+    create_time = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=100)
     upload = models.FileField(upload_to="uploads/%Y/%m/%d/")
     post = models.ForeignKey(Post)
